d54e6d752c3c79d4ce371907e86585f9c6ae0a16
modismit2 <modismit2@gmail.com>
2022-12-29 23:18:06 +0530

    Circle image added

--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,27 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.grey[600],
+        backgroundColor: Colors.grey[800],
+        elevation: 0,
         title: Text(
           ' Flutter Ninja Demo',
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
       ),
-
+      backgroundColor: Colors.grey[850],
+      body: Container(
+        child: Column(
+          children: [
+            Center(
+              child: CircleAvatar(
+// backgroundImage: NetworkImage('https://cdn.pixabay.com/photo/2017/02/08/17/24/fantasy-2049567_960_720.jpg'),
+              backgroundImage: AssetImage('assets/google.png'),
+                ),
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
